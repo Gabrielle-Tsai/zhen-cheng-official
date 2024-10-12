e7b5c82752e2a325d1da55af1e0c28395a490758
Gabrielle Tsai <linda41436@gmail.com>
2024-10-13 01:30:07 +0800

    Add portfolio, client recomments.

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+const swiperPortfolio = new Swiper(".portfolio-swiper", {
+  speed: 400,
+  spaceBetween: 100,
+});
+
+const swiperClient = new Swiper(".client-swiper", {
+  slidesPerView: 1,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 64
+    },
+  },
+  grid: {
+    fill: "columns",
+    rows: 1,
+  },
+});
