a92e2c2ad569a03173d54a2a64c694884d38d818
Gabrielle Tsai <linda41436@gmail.com>
2024-10-16 01:09:54 +0800

    clients comments swiper finished. RWD portfolio swiper desktop finished.

--- main.js
+++ main.js
@@ -2,17 +2,35 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 const swiperPortfolio = new Swiper(".portfolio-swiper", {
+  slidesPerView: 2,
   speed: 400,
   spaceBetween: 100,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true
 });
 
 const swiperClient = new Swiper(".client-swiper", {
   slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    enabled: true,
+  },
 
   breakpoints: {
     768: {
+      slidesPerView: 2,
+      spaceBetween: 64,
+    },
+    992: {
       slidesPerView: 3,
-      spaceBetween: 64
+      spaceBetween: 64,
+      navigation: {
+        enabled: false,
+      },
     },
   },
   grid: {
